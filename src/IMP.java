b853810310451e9e1a5a99589d5cd463a50f214c
BemisJo92 <bidinc.33@gmail.com>
2019-01-23 14:02:56 -0700

    Removed functions.java, might be easier to not have to pass in height and width

--- src/IMP.java
+++ src/IMP.java
@@ -276,22 +276,22 @@ class IMP implements MouseListener{
   
   
 //   // rotate the image ***CLOCKWISE***
-//   private void rotateImage()
-//   {
-// 	  System.out.println("rotateImage called");
-// 	  for(int i=0; i<height; i++)
-// 	       for(int j=0; j<width; j++)
-// 	       {   
-// 	          int rgbArray[] = new int[4];
-	         
-// 	          //get three ints for R, G and B
-// 	          rgbArray = getPixelArray(picture[i][j]);
-	         
-// 	           //take three ints for R, G, B and put them back into a single int
-// 	           picture[i][j] = getPixels(rgbArray);
-// 	        } 
-// 	  resetPicture();
-//   }
+   private void rotateImage()
+   {
+      System.out.println("rotateImage called");
+      for(int i=0; i<height; i++)
+            for(int j=0; j<width; j++)
+            {   
+               int rgbArray[] = new int[4];
+            
+               //get three ints for R, G and B
+               rgbArray = getPixelArray(picture[i][j]);
+            
+               //take three ints for R, G, B and put them back into a single int
+               picture[i][j] = getPixels(rgbArray);
+            } 
+      resetPicture();
+   }
   
 
   
