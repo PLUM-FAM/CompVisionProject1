e927cd3355ef249fc6b4570f7c3e8a21b9191443
joel <hedwigg01@gmail.com>
2019-01-28 20:29:20 -0700

    Histogram Calculations done. Just need to finish drawing. in MyPanel

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -9,20 +9,33 @@ public class MyPanel extends JPanel
  
 int startX, flag, startY, endX, endY;
 
+	int [] freq;
     BufferedImage grid;
     Graphics2D gc;
 
 	public MyPanel()
 	{
-	   startX = startY = 0;
-           endX = endY = 100;
+		startX = startY = 0;
+        endX = endY = 100;
  	}
+	
+	//constructor for drawing histograms.
+	public MyPanel(int[] freq)
+	{
+		startX = startY = 0;
+		endX = endY = 100;
+		
+		this.freq = freq; //initialize global array "freq"		
+	}
 
+	
+	
      public void clear()
     {
        grid = null;
        repaint();
     }
+     
     public void paintComponent(Graphics g)
     {  
          super.paintComponent(g);
@@ -35,6 +48,10 @@ int startX, flag, startY, endX, endY;
 
          }
          g2.drawImage(grid, null, 0, 0);
+         
+         //draw the lines for histogram frequencies
+         //255 lines based off of index and frequency value in freq[]
+         g2.drawLine(0, 0, 100, 100);
      }
     public void drawing()
     {
