4906f1317bed87b52d5d4bc2730f5623898644f3
joel <hedwigg01@gmail.com>
2019-01-30 12:11:03 -0700

    moved histogram drawing to proper drawing() method for use with equalization

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -48,25 +48,24 @@ int startX, flag, startY, endX, endY;
 
          }
          g2.drawImage(grid, null, 0, 0);
+         this.drawing(freq);
          
-         //draw the lines for histogram frequencies
-         //255 lines based off of index and frequency value in freq[]
-         
-         //panel size 305wx600h
-         
-         //print a line for each frequency in freq
-         for (int f = 0; f < 255; f++)
-         {
-        	 g2.drawLine(5+f, this.getHeight(), 5+f, this.getHeight()-freq[f]);
-         }
          
          
          
      }
-    public void drawing()
-    {
+    public void drawing(int freq[])
+    {    	
+    	//draw the lines for histogram frequencies
+        //255 lines based off of index and frequency value in freq[]
         
-        //gc.drawLine(startX, startY, endX, endY);
+        //panel size 305wx600h
+        
+        //print a line for each frequency in freq
+        for (int f = 0; f < 255; f++)
+        {
+       	 gc.drawLine(5+f, this.getHeight(), 5+f, this.getHeight()-freq[f]);
+        }
         repaint();
     }
    
